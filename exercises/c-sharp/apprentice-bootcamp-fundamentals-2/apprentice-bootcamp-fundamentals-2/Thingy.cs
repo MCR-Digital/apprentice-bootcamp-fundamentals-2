5d7671eaf876ecc38ea2538b99cdb150832035a5
Callum Leeming <callum.leeming@zuto.com>
2018-12-08 14:53:48 +0000

    Refactoring, as requested!

--- exercises/c-sharp/apprentice-bootcamp-fundamentals-2/apprentice-bootcamp-fundamentals-2/Thingy.cs
+++ exercises/c-sharp/apprentice-bootcamp-fundamentals-2/apprentice-bootcamp-fundamentals-2/Thingy.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 
 namespace apprentice_bootcamp_fundamentals_2
 {
@@ -30,25 +29,13 @@ namespace apprentice_bootcamp_fundamentals_2
         private string FooFunction()
         {
             foobar = new int[] { 0, 0, 0, 0, 0 }.Length;
-            return ParseHexBinary("42757a7a");
+            return DataTypeConverter.ParseHexBinary("42757a7a");
         }
 
         private string BarFunction()
         {
             bar = 0;
-            return ParseHexBinary("46697a7a");
-        }
-
-        public string ParseHexBinary(string s)
-        {
-            int len = s.Length;
-            byte[] data = new byte[len / 2];
-            for (int i = 0; i < len; i += 2)
-            {
-                data[i / 2] = Convert.ToByte(s.Substring(i, 2), 16);
-            }
-
-            return Encoding.UTF8.GetString(data);
+            return DataTypeConverter.ParseHexBinary("46697a7a");
         }
     }
 }
